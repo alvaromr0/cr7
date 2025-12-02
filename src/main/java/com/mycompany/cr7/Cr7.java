76d7a70c65911edbd57edf46fe458780a12640be
alvaromr0 <amedina@itpfp.com>
2025-12-02 14:52:53 +0100

    subir proyectos

---
+++ src/main/java/com/mycompany/cr7/Cr7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cr7;
+
+/**
+ *
+ * @author Admin
+ */
+public class Cr7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
